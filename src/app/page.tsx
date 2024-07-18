0f8dd74a41793e19560b6a9b28d40d0a2804a307
andretdr <neckoid@hotmail.com>
2024-07-18 23:51:55 +0800

    done some styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,14 +21,16 @@ const CardLink = ()=> {
   return (<>
           {linkInfo.map((item)=>{
             return(
-              <Link className='links' key={item.header} href={item.url}>
-                <div className="card">
+              <div className={`container-sm ${styles.cardlink}`} key={item.header} >
+              <Link className='links' href={item.url}>
+                <div className="card h-75">
                   <div className="card-body">
-                    <h5 className="card-title text-decoration-none">{item.header}</h5>
+                    <h5 className="card-title text-decoration-none display-5">{item.header}</h5>
                     <p className="card-text">{item.body}</p>
                   </div>
                 </div>
               </Link>
+              </div>
             )
           })}
            </>
@@ -40,13 +42,13 @@ const Home = ()=> {
   return (
     <>
         <section id='intro' className="bg-light">
-          <div className="container-sm">
+          <div className='container-lg'>
             <h1 className="display-2 pt-5 pb-2 text-center">A Song of Ice and Fire</h1>
             <p className="fs-5 text-muted pb-5 text-center mb-0">
               Explore the rich universe of author George R. R. Martin</p>
           </div>
         </section>
-        <section id='links' className='bg-secondary'>
+        <section id='links' className='bg-black container-fluid body_height pt-5'>
           <CardLink />
         </section>
     </>
